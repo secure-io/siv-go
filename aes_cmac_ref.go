9513a0bb518aa338e6035084cbf8bf922414eafd
Andreas Auernhammer <aead@mail.de>
2018-09-15 16:42:01 +0200

    rename reference implementation files
    This commit renames generic/reference implementation
    files to follow Go standard lib conventions.

--- aes_cmac_ref.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018 Andreas Auernhammer. All rights reserved.
-// Use of this source code is governed by a license that can be
-// found in the LICENSE file.
-
-// +build !amd64 gccgo appengine
-
-package siv
-
-func newCMAC(key []byte) authEnc { return newCMACGeneric(key) }
